cf2ce4bc2cea22ce191617242da75dd7728260c5
Bashir <bashiralhanshali@yahoo.com>
2020-04-30 15:08:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,22 @@
 # Code your solution here!
+
+def generate_a_number 
+  rand(6) + 1 
+end 
+
+def prompt_user_guess 
+  get 
+end 
+
+def user_input 
+  
+end 
+
+def check_input 
+  
+end 
+
+
+def run_guessing_game 
+
+end 
